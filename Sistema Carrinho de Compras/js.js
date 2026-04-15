515a40f449b41d0edc698363edd6357c5f043127
G4brielaGallo <168927078+G4brielaGallo@users.noreply.github.com>
2026-04-14 23:54:06 -0300

    14/04/2026 23:54
    Aplicação de for para a criação do html interno do areaCompra (poderá ser utilizado como base para o carrinho depois)
    
    Falta:
    CSS
    Carrinho
    Função de Compra do botaoComprar

--- Sistema Carrinho de Compras/js.js
+++ Sistema Carrinho de Compras/js.js
@@ -1,4 +1,4 @@
-localStorage.setItem("nome","André");
+localStorage.setItem("nome", "André");
 const nome = localStorage.getItem("nome");
 console.log(nome); // "André"
 localStorage.removeItem("nome");
@@ -6,20 +6,63 @@ localStorage.clear;
 
 const usuario = JSON.parse(localStorage.getItem("usuario"));
 
+let cont = 0;
+
 const objLivros = {
     9786555320947: {
         titulo: "O Conto da Aia",
         preco: 89.90,
         autor: "Margaret Atwood"
     },
-    ISBN2: {
-        nome: "O Dia do Curinga",
+    9788571645400: {
+        titulo: "O Dia do Curinga",
         preco: 74.90,
         autor: "Jostein Gaarder"
     },
     9788594541475: {
-        nome: "Lady Killers",
+        titulo: "Lady Killers",
         preco: 109.90,
         autor: "Tori Telfer"
     }
 };
+
+const areaCompra = document.querySelector(".areaCompra");
+
+const livros = Object.entries(objLivros);
+
+for (i = 0; i < livros.length; i++) {
+    const [isbn, livro] = livros[i];
+
+    const tituloLivro = document.createElement("p");
+    tituloLivro.innerHTML = livro.titulo;
+    tituloLivro.className = "tituloLivro";
+
+    const autorLivro = document.createElement("p");
+    autorLivro.innerHTML = livro.autor;
+    autorLivro.className = "autorLivro";
+
+    const precoLivro = document.createElement("p");
+    precoLivro.innerHTML = livro.preco;
+    precoLivro.className = "precoLivro";
+
+    const isbnLivro = document.createElement("p");
+    isbnLivro.innerHTML = isbn;
+    isbnLivro.className = "isbnLivro";
+
+    const botaoComprar = document.createElement("button");
+    botaoComprar.innerHTML = "COMPRAR";
+    botaoComprar.className = "btnComprar";
+    botaoComprar.onclick = function comprarLivro() {
+        cont++;
+    }
+
+    const divLivro = document.createElement("div");
+    divLivro.className = "cardLivro";
+    
+    areaCompra.appendChild(divLivro);
+    divLivro.appendChild(tituloLivro);
+    divLivro.appendChild(autorLivro);
+    divLivro.appendChild(precoLivro);
+    divLivro.appendChild(isbnLivro);
+    divLivro.appendChild(botaoComprar);
+}
